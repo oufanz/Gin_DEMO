bc38d1878b1ea384f4cdd3de83bc06074895fd0d
ou_fan <1119498859@qq.com>
2024-08-04 20:44:04 +0800

    2024年8月4日20:44:03

--- config/config.go
+++ config/config.go
@@ -2,9 +2,41 @@ package config
 
 import (
 	"Gin_DEMO/utils/logs"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
+const (
+	TimeFormat string = "2006-01-02 15:04:05"
+)
+
+var (
+	Port string //大写表示其他文件可以访问
+)
+
+func initLogConfig(logLevel string) {
+	//配置日志输出级别
+	if logLevel == "debug" {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+	//加入文件名行号
+	logrus.SetReportCaller(true)
+	//日志格式改为json
+	logrus.SetFormatter(&logrus.JSONFormatter{TimestampFormat: TimeFormat})
+}
 func init() {
 	// fmt.Println("加载程序配置")
 	logs.Debug(nil, "开始加载程序配置")
+	//环境变量加载我们的程序配置
+	viper.SetDefault("LOG_LEVEL", "debug")
+	viper.AutomaticEnv()
+	//配置程序启动端口号
+	viper.SetDefault("PORT", ":8080")
+	logLevel := viper.GetString("LOG_LEVEL") //获取程序配置
+	Port = viper.GetString("PORT")
+	//加载日志输出格式
+	initLogConfig(logLevel)
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Gin_DEMO/config"
 	_ "Gin_DEMO/config" //导入包但不使用里面的方法
 	"Gin_DEMO/utils/logs"
 
@@ -11,6 +12,6 @@ func main() {
 	//1.加载程序配置:有方法叫做initial，自动调用
 	logs.Info(nil, "程序启动！")
 	r := gin.Default() //1.初始化，创建一个路由引擎，可以
-	r.Run()            //阻塞
+	r.Run(config.Port) //阻塞
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
